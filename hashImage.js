0b4c7badab94a79f8acaa23d598d2b27800702d1
Visheshgpt <guptavishesh98@gmail.com>
2022-08-29 11:40:25 +0530

    add comments

--- hashImage.js
+++ hashImage.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const fileBuffer = fs.readFileSync("./images/1.png");
 console.log(fileBuffer);
 x = Buffer.isBuffer(fileBuffer) 
+
+// check if the image is empty or not
 y = fileBuffer.length
 console.log(x)
 console.log(y);;
